05eb79608df9af1d7cbd2c69bf9ea72f6f5d3761
davidajoyce <davidajoyce141@gmail.com>
2019-02-04 20:43:11 +0000

    initial commit showing a Landing Page along with four links and a menu

---
+++ myportfoliosite/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl'
+import  Main  from './Components/main';
+import { Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+    <Layout>
+        <Header title="Title" scroll>
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+    </Layout>
+</div>
+
+    );
+  }
+}
+
+export default App;
---
+++ myportfoliosite/src/Components/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import LandingPage from './landingpage';
+
+
+const Main = () => (
+  <Switch>
+    <Route exact path = "/" component={LandingPage} />
+  </Switch>
+)
+
+export default Main;
