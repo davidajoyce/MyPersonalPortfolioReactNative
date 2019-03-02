895fc43973b35fb7f642fe6f07aba5230d77412a
davidajoyce <davidajoyce141@gmail.com>
2019-03-02 17:51:10 +0000

    include job role title, skills and link icons

--- myportfoliosite/src/Components/landingpage.js
+++ myportfoliosite/src/Components/landingpage.js
@@ -1,9 +1,51 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class LandingPage extends Component{
   render(){
       return(
-        <div><h1>Landing Page</h1></div>
+        <div style={{width: '100%', margin: 'auto'}}>
+          <Grid className="landing-grid">
+            <Cell col={12}>
+              <img
+                src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/malecostume-512.png"
+                alt="avatar"
+                className="avatar-img"
+                />
+
+                <div className="banner-text">
+                   <h1>Full Stack Web Developer</h1>
+
+                <hr/>
+                <p>Java | Python | C#  | React Native | Bash | SQL </p>
+
+              <div className="social-links">
+
+                {/*LinkedIn*/}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+
+                {/*Github*/}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+
+                {/*Freecodecamp*/}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-free-code-camp" aria-hidden="true" />
+                </a>
+
+                {/*Youtube*/}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-youtube-square" aria-hidden="true" />
+                </a>
+
+              </div>
+                </div>
+            </Cell>
+          </Grid>
+        </div>
       )
     }
 }
