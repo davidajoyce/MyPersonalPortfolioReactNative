db79a553a2d5840c67e98225c5658d3981186f8a
davidajoyce <davidajoyce141@gmail.com>
2019-03-03 18:40:21 +0000

    added half page grid with icon and icon list

--- myportfoliosite/src/Components/contact.js
+++ myportfoliosite/src/Components/contact.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component{
   render(){
       return(
-        <div><h1>Contact Page</h1></div>
+        <div className="contact-body">
+          <Grid className="contact-grid">
+            <Cell col={6}>
+              <h2>David Joyce</h2>
+              <img
+                 src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                 alt="avatar"
+                 style={{height: '250px'}}
+                 />
+                 <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>
+                 This is example text to be filled allalalallajddiirjd</p>
+            </Cell>
+            <Cell col={6}>
+              <h2>Contact Me</h2>
+              <hr/>
+
+              <List>
+                <ListItem>
+                 <ListItemContent icon="person">Bryan Cranston</ListItemContent>
+                </ListItem>
+                <ListItem>
+                 <ListItemContent icon="person">Aaron Paul</ListItemContent>
+                </ListItem>
+                <ListItem>
+                 <ListItemContent icon="person">Bob Odenkirk</ListItemContent>
+                </ListItem>
+              </List>
+
+            </Cell>
+          </Grid>
+        </div>
       )
     }
 }
