7ddc62f68296d810971a48b9117b28456449c460
davidajoyce <davidajoyce141@gmail.com>
2019-03-03 17:06:17 +0000

    add tabs to the project page with text

--- myportfoliosite/src/Components/projects.js
+++ myportfoliosite/src/Components/projects.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Projects extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+
+    if(this.state.activeTab === 0){
+      return(
+        <div><h1>This is React</h1></div>
+      )
+    } else if(this.state.activeTab === 1){
+      return(
+        <div><h1>This is Angular</h1></div>
+      )
+    } else if(this.state.activeTab === 2){
+      return(
+        <div><h1>This is VueJS</h1></div>
+      )
+    } else if(this.state.activeTab === 3){
+      return(
+        <div><h1>This is MongoDB</h1></div>
+      )
+    }
+  }
+
   render(){
       return(
-        <div><h1>Projects</h1></div>
+        <div className="category-tabs">
+           <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+              <Tab>React</Tab>
+              <Tab>Angular</Tab>
+              <Tab>VueJS</Tab>
+              <Tab>MongoDB</Tab>
+           </Tabs>
+
+           <section className="projects-grid">
+             {this.toggleCategories()}
+           </section>
+        </div>
       )
     }
 }
